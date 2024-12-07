15a872da7c82f79132ff210201ad9607c903b6f1
devadityadev <raghavendra91440@gmail.com>
2024-12-07 08:11:59 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,13 +1,3 @@
-// JavaScript for smooth scrolling (optional)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
 // JavaScript to toggle the mobile menu visibility
 const menuToggle = document.getElementById('mobile-menu');
 const navLinks = document.getElementById('nav-links');
